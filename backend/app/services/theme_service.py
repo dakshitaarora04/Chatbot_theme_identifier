85c660d93dd13b9382d67af633f1937c544f7fcc
Dakshita Arora <dakshitaarora04@gmail.com>
2025-05-20 20:25:27 +0530

    Update theme_service.py

--- backend/app/services/theme_service.py
+++ backend/app/services/theme_service.py
@@ -1,10 +1,17 @@
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+#similar text are grouped as chunks from documents into themes using unsupervised machine learning
+#Each chunk of text is converted into a TF-IDF vector (a numerical representation that reflects word importance)
+#Now the text converts into a format that a machine learning algorithm can understand.
+
+
 def identify_themes(chunks, max_clusters=5):
     if not chunks:
         return [], []
-
+        
+#K-Means clustering is applied to group similar vectors
+    
     vectorizer = TfidfVectorizer(stop_words='english')
     X = vectorizer.fit_transform(chunks)
 
