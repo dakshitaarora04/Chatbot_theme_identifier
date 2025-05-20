d451e01027a6d3992cbf749bcf41e03f6e388ed3
Dakshita Arora <dakshitaarora04@gmail.com>
2025-05-20 20:31:56 +0530

    Update embedding_model.py

--- backend/app/models/embedding_model.py
+++ backend/app/models/embedding_model.py
@@ -1,5 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
+#this model transforms sentences into numeric vectors(embedding).
+
 def load_embedding_model():
     model = SentenceTransformer('all-MiniLM-L6-v2')
     return model
